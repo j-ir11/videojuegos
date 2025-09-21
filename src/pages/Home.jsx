bdf33e6d67292ef4f936b543c224b535253ef1bf
Jair <ibarrajair30@gmail.com>
2025-09-20 22:24:52 -0600

    eliminar nombres

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -216,7 +216,7 @@ export default function Home() {
   
             {/* Derechos */}
             <div className="text-sm text-gray-500 dark:text-gray-400">
-              &copy; {new Date().getFullYear()} JamedGames. Creado por Jair, Amed y Jemmy
+              &copy; {new Date().getFullYear()} JamedGames. 
             </div>
   
             {/* Redes sociales */}
